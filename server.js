7213046bf27020806bfa378926f2f7d9abaa695b
Mohamed <94723351+Mohame0136@users.noreply.github.com>
2022-06-07 20:23:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require('express')
 const app = express()
-const userSchema = require('./models/User')
+const userSchema = require('./models/person')
 const connectDB = require('./config/ConnectDB')
 require('dotenv').config()
 
 connectDB()
 
-let user = User.model('User', userSchema)
+let person = person.model('person', personSchema)
 
 /*create a new user */
 
-var user1 = new user({
-  name:'user0',
+var person1 = new person({
+  name:'personr0',
   age:'20'
 });
 
-user1.save();
+person1.save();
 
 /* Create Many Records */
 
-user.create(
+person.create(
   [
     {
-      name: 'user1',
+      name: 'person1',
       age: 10,
       favoriteFoods: ['makrouna'],
     },
     {
-      name: 'user2',
+      name: 'person2',
       age: 15,
       favoriteFoods: ['chakchouka', 'sandwich'],
     },
     {
-      name: 'user3',
+      name: 'person3',
       age: 20,
       favoriteFood: ['lasagne', 'meat'],
     },
@@ -41,22 +41,21 @@ user.create(
     if (err) {
       return console.log.error(err)
     }
-    return console.log('users are created')
+    return console.log('persons are created')
   },
 )
 
-/* Find users with names  */
+/* Find persons with names  */
 
-user.find({name: 'user1'}).then( (data) => { console.log(data) })
-//same with all the oyher users
+person.find({name: 'person1'}).then( (data) => { console.log(data) })
 
 
-/* Find user with favoritefood */
-user.findOne( {favoriteFood: 'Pasta',} ).then( (data) => { console.log(data) })
+/* Find person with favoritefood */
+person.findOne( {favoriteFood: 'Pasta',} ).then( (data) => { console.log(data) })
 
 
 /* Find with _id */
-user.findById('7c83056383e874948c6383').then( (data) => { console.log(data) })
+person.findById('7c83056383e874948c6383').then( (data) => { console.log(data) })
 
 /*Find with _id and add food*/
 
@@ -75,7 +74,7 @@ user.findById('7c83056383e874948c6383', (err, call) => {
 })
 
 /* Find with Name and change the age */
-user.findOneAndUpdate({ name: 'user2' },{ $set: { age: 10 } },{ new: true },
+person.findOneAndUpdate({ name: 'person2' },{ $set: { age: 10 } },{ new: true },
   (err, data) => {
     if (err) {
       console.log('error')
@@ -85,17 +84,17 @@ user.findOneAndUpdate({ name: 'user2' },{ $set: { age: 10 } },{ new: true },
   },
 )
 
-/* Delete user witth id */
-user.findByIdAndRemove('7c83056383e874948c6383', function (err, data) {
+/* Delete witth id */
+person.findByIdAndRemove('7c83056383e874948c6383', function (err, data) {
   if (err) {
     console.log(err)
   } else {
-    console.log('Removed User : ', data)
+    console.log('Removed person : ', data)
   }
 })
 
-/*Delete all the people whose name is “emna”*/
-user.remove({ name: 'user3' }, (err, data) => {
+/*Delete*/
+person.remove({ name: 'person3' }, (err, data) => {
   if (err) {
     console.log(err)
   } else {
